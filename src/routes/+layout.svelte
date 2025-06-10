4615ad2772fed4c7941b7979b2892072ac22c560
MaxBTD <97893081+MaxBTD@users.noreply.github.com>
2025-06-10 20:52:12 +0200

    v0.1 page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-    export const prerender = true;
+        export const prerender = true;
 
     import { bounceOut } from "svelte/easing";
     import { elasticOut } from "svelte/easing";
